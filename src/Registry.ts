9efc933077702ea80fd5034a1eef88689d51abaa
Nathan King <nathanking3590@gmail.com>
2021-11-12 13:37:55 +0000

    Rename Registry constructor arg

--- src/Registry.ts
+++ src/Registry.ts
@@ -3,14 +3,14 @@ import { CommandMappingError } from "./CommandMappingError";
 import { Handler } from "./Handler";
 
 export class Registry {
-    private registry: [string, Handler][];
+    private mappings: [string, Handler][];
 
-    constructor(registry: [string, Handler][]) {
-        this.registry = registry;
+    constructor(mappings: [string, Handler][]) {
+        this.mappings = mappings;
     }
 
     resolve(command: Command): Handler {
-        const resolved = this.registry.find((mapping: [string, Handler]) => mapping[0] === command.getName());
+        const resolved = this.mappings.find((mapping: [string, Handler]) => mapping[0] === command.getName());
 
         if (!resolved) {
             throw new CommandMappingError(`Command ${command.getName()} could not be resolved. It might not be mapped to a handler.`);
