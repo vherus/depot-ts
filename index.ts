a8a16f640f84cc8059ebdd39868d19a9f95a148a
Nathan King <nathanking3590@gmail.com>
2021-12-20 14:31:34 +0000

    Move index.ts

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { Command } from './src/Command';
+export { CommandBus } from './src/CommandBus';
+export { CommandMappingError } from './src/CommandMappingError';
+export { Depot } from './src/Depot';
+export { Handler } from './src/Handler';
+export { Registry } from './src/Registry';
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { Command } from './Command';
-export { CommandBus } from './CommandBus';
-export { CommandMappingError } from './CommandMappingError';
-export { Depot } from './Depot';
-export { Handler } from './Handler';
-export { Registry } from './Registry';
